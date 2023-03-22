08773d2667ba048d88cc8f2b9a2ddf1568d82e5c
Robert Greenawalt <robert.greenawalt29@gmail.com>
2023-03-22 16:54:04 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World App')
+})
+
+app.listen(3000, () => { console.log('App is listening on port 3000') })
