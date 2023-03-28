5e824777b6da0b21dc6d3b37e02f98fcea98b8eb
Robert Greenawalt <robert.greenawalt29@gmail.com>
2023-03-28 16:07:09 -0400

    Trivial change

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const app = express()
+const app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World App')
 })
 
-app.listen(3000, () => { console.log('App is listening on port 3000') })
+app.listen(3000, () => { console.log('App is listening on PORT 3000') })
